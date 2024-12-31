1cd15ac1a272b0de81aaa703c5b80ceac8e7179a
Ayaan-Sheik <ayaansheik332@gmail.com>
2025-01-01 02:30:27 +0530

    Edit script.js

--- script.js
+++ script.js
@@ -21,7 +21,67 @@ function getHumanChoice(a){
         }else{
             console.log("Enter proper number");
         }
-    }while(!(a.toLowerCase() === "rock"||"paper"||"scissors"));
+    }while(!(a.toLowerCase() === "rock"||"paper"||"scissors"));   
+}
+
+function playRound(humanChoice,computerChoice){
+    if (humanChoice == "Rock") {
+        if(computerChoice == "Rock"){
+            console.log("The Round is a Tie!!");
+            return "tie";
+        } 
+        else if (computerChoice == "Paper") {
+            console.log("Computer Wins the Round!!");
+            return "Computer";
+        }
+        else{
+            console.log("Human Wins the Round!!");
+            return "Human";
+        }
+    }
+
+    if (humanChoice == "Paper") {
+        if(computerChoice == "Rock"){
+            console.log("Human Wins the Round!!");
+            return "Human";
+        } 
+        else if (computerChoice == "Paper") {
+            console.log("The Round is a Tie!!");
+            return "tie";
+        }
+        else{
+            console.log("Computer Wins the Round!!");
+            return "Computer";
+        }
+    }
+
+    if (humanChoice == "Scissors") {
+        if(computerChoice == "Rock"){
+            console.log("Computer Wins the Round!!");
+            return "Computer";
+        } 
+        else if (computerChoice == "Paper") {
+            console.log("Human Wins the Round!!");
+            return "Human";
+        }
+        else{
+            console.log("The Round is a Tie!!");
+            return "tie";
+        }
+    }
+}
+
+let round = 1;
+let human = 0;
+let computer = 0;
+
+while (round<=5){
+    console.log("This is the" +round);
+    let k = prompt("Rock,Paper,Scissors","--Rock--");
+    let n = playRound(getHumanChoice(k),getComputerChoice());
     
-        
+    if(n === "Human") human++;
+    else if(n === "Computer") computer++;
+    console.log(`Human :${human} :: Computer:${computer}`);
+    round++;
 }
